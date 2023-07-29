d82050233c1bd99d48bdca5f833ad63e4a262635
kipngenohaaron <kipngenohaaron@gmail.com>
2023-07-30 00:14:02 +0300

    records updated

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,28 +1,13 @@
 import React from 'react';
+import BotCard from './BotCard';
 
-const YourBotArmy = () => {
-  // State to hold enlisted bots
-  const [enlistedBots, setEnlistedBots] = useState([]);
-
-  const handleRemoveFromArmy = (botId) => {
-    // Logic to remove a bot from the enlistedBots array
-    setEnlistedBots(enlistedBots.filter((bot) => bot.id !== botId));
-  };
-
+const YourBotArmy = ({ army, removeFromArmy }) => {
   return (
-    <div>
-      <h1>Your Bot Army</h1>
-      <div>
-        {enlistedBots.map((bot) => (
-          <div key={bot.id}>
-            <h2>{bot.name}</h2>
-            <p>Health: {bot.health}</p>
-            <p>Damage: {bot.damage}</p>
-            <p>Armor: {bot.armor}</p>
-            <p>Class: {bot.bot_class}</p>
-            <button onClick={() => handleRemoveFromArmy(bot.id)}>Release</button>
-            <hr />
-          </div>
+    <div className="bot-army">
+      <h2>Your Bot Army</h2>
+      <div className="bot-list">
+        {army.map((bot) => (
+          <BotCard key={bot.id} bot={bot} removeFromArmy={removeFromArmy} />
         ))}
       </div>
     </div>
